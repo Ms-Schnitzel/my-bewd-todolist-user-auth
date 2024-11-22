a58b619568a74f275dd879bf1d127aca2ddd5c57
Spartan N7 <zachnielsen72@gmail.com>
2024-11-22 05:30:08 -0700

    missing tasks code

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_11_22_015434) do
+ActiveRecord::Schema.define(version: 2024_11_22_122653) do
 
   create_table "sessions", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 2024_11_22_015434) do
     t.datetime "updated_at", null: false
     t.string "content"
     t.boolean "completed", default: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +36,5 @@ ActiveRecord::Schema.define(version: 2024_11_22_015434) do
     t.string "password"
   end
 
+  add_foreign_key "tasks", "users"
 end
